7f8ede16f986ee7de656bb23ccec1784fece18dc
antimonyB <anthony.bertnyk@gmail.com>
2021-06-12 16:40:57 -0400

    Differentiate swipe and tap to spawn different items

--- Assets/Scripts/AnchorCreator.cs
+++ Assets/Scripts/AnchorCreator.cs
@@ -6,11 +6,22 @@ using UnityEngine.XR.ARFoundation;
 // Modified from AR Core Foundation Samples
 [RequireComponent(typeof(ARAnchorManager))]
 [RequireComponent(typeof(ARRaycastManager))]
-public class AnchorCreator : MonoBehaviour {
+public class AnchorCreator : MonoBehaviour
+{
     public GameObject wall;
     public GameObject tower;
 
-    bool isTowerNext = false;
+    //bool isTowerNext = false;
+    bool isSwiping = false;
+    bool hasTappedItem = false;
+
+
+    Pose startPose;
+    Vector2 startTouchPos;
+    Transform objectToGrow;
+
+    readonly float GROW_HEIGHT = 0.1f;
+    readonly float SWIPE_THRESH = 40;
 
     static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
     List<ARAnchor> m_Anchors = new List<ARAnchor>();
@@ -18,76 +29,121 @@ public class AnchorCreator : MonoBehaviour {
     ARAnchorManager m_AnchorManager;
     Camera arCamera;
 
-    public void RemoveAllAnchors() {
-        foreach (var anchor in m_Anchors) {
+    public void RemoveAllAnchors()
+    {
+        foreach (var anchor in m_Anchors)
+        {
             Destroy(anchor.gameObject);
         }
         m_Anchors.Clear();
     }
 
-    void Awake() {
+    void Awake()
+    {
         m_RaycastManager = GetComponent<ARRaycastManager>();
         m_AnchorManager = GetComponent<ARAnchorManager>();
         arCamera = GetComponentInChildren<Camera>(); // Gets the attached AR Camera from the AR Session Origin
     }
 
-    ARAnchor CreateAnchor(in ARRaycastHit hit) {
+    ARAnchor CreateAnchor(in ARRaycastHit hit)
+    {
         ARAnchor anchor = null;
 
-        GameObject prefab = isTowerNext ? tower : wall;
-        isTowerNext = !isTowerNext;
+        GameObject prefab = tower;
+        Pose spawnPose = hit.pose;
+        if (isSwiping) {
+            prefab = wall;
+            spawnPose = startPose;
+            spawnPose.position = (startPose.position + hit.pose.position) / 2;
+        }
 
         // If we hit a plane, try to "attach" the anchor to the plane
-        if (hit.trackable is ARPlane plane) {
+        if (hit.trackable is ARPlane plane)
+        {
             var planeManager = GetComponent<ARPlaneManager>();
-            if (planeManager) {
+            if (planeManager)
+            {
                 m_AnchorManager.anchorPrefab = prefab;
-                anchor = m_AnchorManager.AttachAnchor(plane, hit.pose);
+                anchor = m_AnchorManager.AttachAnchor(plane, spawnPose);
                 return anchor;
             }
         }
 
         // Note: the anchor can be anywhere in the scene hierarchy
-        var gameObject = Instantiate(prefab, hit.pose.position, hit.pose.rotation);
+        var gameObject = Instantiate(prefab, spawnPose.position, spawnPose.rotation);
 
         // Make sure the new GameObject has an ARAnchor component
         anchor = gameObject.GetComponent<ARAnchor>();
-        if (anchor == null) {
+        if (anchor == null)
+        {
             anchor = gameObject.AddComponent<ARAnchor>();
         }
 
         return anchor;
     }
 
-    void Update() {
+    void Update()
+    {
         if (Input.touchCount == 0)
             return;
 
         var touch = Input.GetTouch(0);
-        if (touch.phase != TouchPhase.Began)
-            return;
+        if (touch.phase == TouchPhase.Began)
+        {
+            hasTappedItem = false;
+            isSwiping = false;
 
-        //// Raycast against planes and feature points
-        //const TrackableType trackableTypes = TrackableType.All;
-        ////TrackableType.FeaturePoint |
-        ////TrackableType.PlaneWithinPolygon;
+            //// Raycast against planes and feature points
+            //const TrackableType trackableTypes = TrackableType.All;
+            ////TrackableType.FeaturePoint |
+            ////TrackableType.PlaneWithinPolygon;
 
-        RaycastHit physicsHit;
-        Ray ray = arCamera.ScreenPointToRay(touch.position);
+            RaycastHit physicsHit;
+            Ray ray = arCamera.ScreenPointToRay(touch.position);
 
-        // Perform the raycast
-        if (Physics.Raycast(ray, out physicsHit) && !physicsHit.transform.GetComponent<ARPlane>()) {
-            Transform transformHit = physicsHit.transform;
-            transformHit.localScale = new Vector3(transformHit.localScale.x, transformHit.localScale.y, transformHit.localScale.z * 2);
-        } else if (m_RaycastManager.Raycast(touch.position, s_Hits, TrackableType.All)) {
-            // Raycast hits are sorted by distance, so the first one will be the closest hit.
-            var hit = s_Hits[0];
+            // Perform the raycast
+            if (Physics.Raycast(ray, out physicsHit) && !physicsHit.transform.GetComponent<ARPlane>())
+            {
+                objectToGrow = physicsHit.transform;
+                hasTappedItem = true;
+            }
+            if (m_RaycastManager.Raycast(touch.position, s_Hits, TrackableType.All))
+            {
+                // Raycast hits are sorted by distance, so the first one will be the closest hit.
+                var hit = s_Hits[0];
 
-            // Create a new anchor
-            var anchor = CreateAnchor(hit);
-            if (anchor) {
-                // Remember the anchor so we can remove it later.
-                m_Anchors.Add(anchor);
+                // Remember location of tap in case this is a swipe
+                startPose = hit.pose;
+                startTouchPos = touch.position;
+            }
+        }
+        else if (touch.phase == TouchPhase.Ended)
+        {
+            if ((touch.position - startTouchPos).magnitude > SWIPE_THRESH)
+            {
+                isSwiping = true;
+            }
+
+            if (hasTappedItem && !isSwiping)
+            {
+                objectToGrow.localScale = new Vector3(objectToGrow.localScale.x, objectToGrow.localScale.y + GROW_HEIGHT, objectToGrow.localScale.z);
+                objectToGrow.localPosition = new Vector3(objectToGrow.localPosition.x, objectToGrow.localPosition.y + GROW_HEIGHT / 2, objectToGrow.localPosition.z);
+            }
+            else
+            {
+                if (m_RaycastManager.Raycast(touch.position, s_Hits, TrackableType.All))
+                {
+                    // Raycast hits are sorted by distance, so the first one will be the closest hit.
+                    var hit = s_Hits[0];
+
+                    // Create a new anchor
+                    var anchor = CreateAnchor(hit);
+                    if (anchor)
+                    {
+                        // Remember the anchor so we can remove it later.
+                        m_Anchors.Add(anchor);
+                    }
+                }
             }
         }
     }
